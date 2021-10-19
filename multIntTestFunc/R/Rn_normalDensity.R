a49b63578e36e754930714eebbc95e26034a5b4b
Klaus Herrmann <herk1401@dmat-herrma-01i.DMat.fsci.usherbrooke.ca>
2021-10-18 20:49:45 -0400

    Adds log-normal density and improves product rule function.

--- multIntTestFunc/R/Rn_normalDensity.R
+++ multIntTestFunc/R/Rn_normalDensity.R
@@ -1,24 +1,24 @@
 
 #Rn_normalDensity function
 
-#' An S4 class to represent the function \eqn{\frac{1}{\sqrt{(2\pi)^d\det(\Sigma)}}\exp(-((\vec{x}-\vec{\mu})^{\tr}\Sigma^{-1}(\vec{x}-\vec{\mu}))/2)} on \eqn{R^n}
+#' An S4 class to represent the function \eqn{\frac{1}{\sqrt{(2\pi)^n\det(\Sigma)}}\exp(-((\vec{x}-\vec{\mu})^{T}\Sigma^{-1}(\vec{x}-\vec{\mu}))/2)} on \eqn{R^n}
 #'
 #' Implementation of the function
-#' \deqn{f \colon R^n \to (0,\infty),\, \vec{x} \mapsto f(\vec{x}) = \frac{1}{\sqrt{(2\pi)^d\det(\Sigma)}}\exp(-((\vec{x}-\vec{\mu})^{\tr}\Sigma^{-1}(\vec{x}-\vec{\mu}))/2),}
+#' \deqn{f \colon R^n \to (0,\infty),\, \vec{x} \mapsto f(\vec{x}) = \frac{1}{\sqrt{(2\pi)^n\det(\Sigma)}}\exp(-((\vec{x}-\vec{\mu})^{\tr}\Sigma^{-1}(\vec{x}-\vec{\mu}))/2),}
 #' where \eqn{n \in \{1,2,3,\ldots\}} is the dimension of the integration domain \eqn{R^n = \times_{i=1}^n R}.
 #' In this case the integral is know to be
 #' \deqn{\int_{R^n} f(\vec{x}) d\vec{x} = 1.}
 #'
 #' The instance needs to be created with three parameters representing the dimension \eqn{n}, the location vector \eqn{\vec{\mu}} and the variance-covariance matrix \eqn{\Sigma} which needs to be symmetric positive definite.
 #' @slot dim An integer that captures the dimension
-#' @slot mean A vector of size dim with real entries. Default is rep(0,dim)
-#' @slot sigma A matrix of size dim x dim that is symmetric positive definite. Default is diag(dim)
+#' @slot mean A vector of size dim with real entries.
+#' @slot sigma A matrix of size dim x dim that is symmetric positive definite.
 #' @include AllGeneric.R
 #' @export Rn_normalDensity
 #' @exportClass Rn_normalDensity
 #' @examples
 #' n <- as.integer(3)
-#' f <- new("Rn_normalDensity",dim=n)
+#' f <- new("Rn_normalDensity",dim=n,mean=rep(0,n),sigma=diag(n))
 Rn_normalDensity <- setClass(Class="Rn_normalDensity",representation=representation(dim="integer",mean="vector",sigma="matrix"))
 
 #' @rdname exactIntegral
--- multIntTestFunc/R/misc.R
+++ multIntTestFunc/R/misc.R
@@ -10,43 +10,60 @@
 #  }
 #}
 
-
 #' Product rule for numerical quadrature from univariate nodes and weights
 #'
-#' @param dim An integer that defines the dimension of the output quadrature formula
-#' @param nodes A vector with univariate node points
-#' @param weights A vector with univariate weights
+#' @param x Either a list with two elements $nodes and $weights representing a one dimensionsal quadrature formula which are used for all dimensions, or a list where each element is a itself a list with two elements $nodes and $weights. Then for each dimension the respective quadrature rule is used.
+#' @param dim An integer that defines the dimension of the output quadrature formula. Default is NULL. If dim is NULL then x has to be a list of quadrature rules (list of lists) and the dimensions is automatically generated.
 #' @return A list with a matrix of multivariate node points (each row is one point) and a vector of corresponding weights
 #' @examples
 #' require(statmod)
-#' N <- 4; kind <- "hermite"
-#' gauss <- gauss.quad(N,kind=kind)
-#' w <- gauss$weights
-#' x <- gauss$nodes
-#' multQuad <- quadratureProductRule(2,x,w)
+#' herm <- gauss.quad(2,"hermite")
+#' lag <- gauss.quad(3,"laguerre")
+#' qRule1 <- pIntRule(herm,2)
+#' qRule2 <- pIntRule(list(herm,lag))
 #' @export
-quadratureProductRule <- function(dim,nodes,weights){
-
-    w <- weights
-    x <- nodes
-
-    l <- rep(list(1:N), dim)
-    ind <- expand.grid(l)
-
-    nn <- nrow(ind)
-
-    nodes <- matrix(NaN,nn,dim)
-    weights <- rep(NaN,nn)
-        
-    for (k in 1:nn) {
-        indk <- as.numeric(ind[k,]) 
-        nodes[k,] <- x[indk]
-        weights[k] <- prod(w[indk])
+pIntRule <- function(x,dim=NULL){
+  stopifnot(is.null(dim)||(dim>=1))
+  
+  if (is.null(dim)){
+    qRule <- x
+    dim <- length(qRule)
+  } else {
+    qRule <- vector("list",dim)
+    for (k in 1:dim) {
+      qRule[[k]] <- x
     }
-
-    L <- list(nodes=nodes,weights=weights)
+    
+  }
+  
+  indList <- vector("list",dim)
+  
+  for (k in 1:dim) {
+    indList[[k]] <- 1:length(qRule[[k]]$nodes)
+  }
+  
+  ind <- expand.grid(indList)
+  nn <- nrow(ind)
+  
+  nodes <- matrix(NaN,nn,dim)
+  weights <- rep(NaN,nn)
+  
+  for (k in 1:nn) {
+    indk <- as.numeric(ind[k,])
+    
+    node <- rep(NA,dim)
+    logWeight <- 0
+    
+    for (l in 1:dim){
+      i <- indk[l]
+      node[l] <- qRule[[l]]$nodes[i]
+      logWeight <- logWeight + log(qRule[[l]]$weights[i])
+    }
+    
+    nodes[k,] <- node
+    weights[k] <- exp(logWeight)
+  }
+  
+  L <- list(nodes=nodes,weights=weights)
 }
-
-
-
 
